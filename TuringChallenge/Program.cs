3aaf81eb1978c08f8222146c0c700d6e7ea1a370
Jhonatan Tirado <jhonatantiradotiradodeep@gmail.com>
2023-03-30 12:50:52 -0500

    Frist commit

---
+++ TuringChallenge/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace CalPoints
+{
+    class Solution
+    {
+        public static int CalPoints(string[] ops)
+        {
+            var scores = new List<string>();
+            var total = 0;
+
+            foreach (var item in ops)
+            {
+                if (item.Equals("+"))
+                {
+
+                    var newScore = int.Parse(scores[scores.Count - 1]) + int.Parse(scores[scores.Count - 2]);
+                    scores.Add(newScore.ToString());
+
+                }
+                else if (item.Equals("D"))
+                {
+
+                    var newScore = int.Parse(scores[scores.Count - 1]) * 2;
+                    scores.Add(newScore.ToString());
+
+                }
+                else if (item.Equals("C"))
+                {
+
+                    scores.RemoveAt(scores.Count - 1);
+
+                }
+                else if (int.TryParse(item, out _))
+                {
+
+                    scores.Add(item);
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+
+            foreach (var score in scores)
+            {
+                total += int.Parse(score);
+            }
+
+            return total;
+        }
+    }
+
+    class CalPoints
+    {
+        public static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var space = new char[] { ' ' };
+
+            string[] ops = Console.ReadLine().Split(space);
+            int output = Solution.CalPoints(ops);
+            Console.Write(output.ToString());
+        }
+    }
+
+}
